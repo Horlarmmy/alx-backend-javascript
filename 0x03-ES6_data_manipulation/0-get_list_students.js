cc6ce0452701193dc2791eee7d7c8cb2af293feb
Alade Toheeb <aladetoheeb5@gmail.com>
2023-01-03 17:31:19 +0100

    Task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const s_list = [];
+  s_list.push({'id': 1, 'firstName':'Guillaume','location':'San Francisco'})
+  s_list.push({'id': 2, 'firstName':'James','location':'Columbia'})
+  s_list.push({'id': 5, 'firstName':'Serena','location':'San Francisco'})
+  return s_list;
+}
