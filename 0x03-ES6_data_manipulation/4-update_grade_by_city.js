22e3a5b54288f09cd30d3a1c8e5c2f8a3a412a54
Alade Toheeb <aladetoheeb5@gmail.com>
2023-01-03 22:51:31 +0100

    Task 5 and 6

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,3 @@
+export default function updateStudentGradeByCity(array1, city, array2) {
+  const gradeCity = array1.filter(x => x.location === city).map(x => ;
+}
---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,4 @@
+export default function setFromArray(array1) {
+  const newSet = new Set(array1);
+  return newSet;
+}
